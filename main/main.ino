03762be33c404aacf743ebb767d7da4e888f415d
henrynester <henrynester@gmail.com>
2018-03-06 12:03:04 -0500

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -25,6 +25,42 @@ byte count = 0;
 //Communication-related
 byte numRegisters = 10; //some number
 uint16_t modbusRegisters[numRegisters] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+/*Modbus Register contents (add more as needed, these are the bare minimum to control robot functions
+The library requires an array of UNsigned 16-bit integers, but we can use them as needed
+The purpose of each element of the array is listed by index here:
+//=============Fast loop data (High priority) =================
+0. Thruster Speed #1 (signed 16-bit integers. Negative is reverse, positive is forward)
+1. Thruster Speed #2 (Can be sent directly to ESCs over i2c bus)
+2. Thruster Speed #3
+3. Thruster Speed #4
+4. Thruster Speed #5
+5. Thruster Speed #6
+6. Manipulator Speeds #1, #2 (8-bit signed integer. High byte controls one motor, 
+                              low byte controls other motor)
+7. Manipulator Speeds #3, #4 (Negative is reverse, positive is forward, abs value is speed
+                             see pindefs.h for guidelines controlling manipulators)
+8. Voltage @ ROV (see pindefs.h for analog pin to use)
+9. Yaw (these are floating points ranging -180 <-> 180 degrees. We don't need more than one decimal
+        place precision)
+10. Pitch
+11. Roll
+12. Depth Reading (32-bit value needing complicated conversion at surface computer)
+13. Depth Reading part two
+//==============Slow loop data (Low priority)================
+14. Thruster RPM #1
+15.Thruster RPM #2
+16.Thruster RPM #3
+17.Thruster RPM #4
+18.Thruster RPM #5
+19.Thruster RPM #6
+20. Thruster Temp #1
+21.Thruster Temp #2
+22.Thruster Temp #3
+23.Thruster Temp #4
+24.Thruster Temp #5
+25.Thruster Temp #6
+//more exist. see slack post with listing in #programming dated Feb 25th
+
 
 //Object instantiations
 //slave address 1, use Arduino serial port, TX_EN pin is defined in pindefs.h file
