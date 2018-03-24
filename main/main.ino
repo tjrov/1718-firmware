6476e8c34e6069032f665eda43b9d817f993486d
henrynester <henrynester@gmail.com>
2018-03-24 17:45:28 -0400

    added setManipulator() function

--- main/main.ino
+++ main/main.ino
@@ -141,3 +141,19 @@ void fastLoop() {
 void slowLoop() {
   //runs 10 times / second
 }
+//function takes a signed 8-bit integer for the speed (range -128<->127)
+//negative values go reverse, positive forwards
+//other two params are the pins to use (use pindefs.h constants please)
+void setManipulator(int8_t val, byte dir1, byte dir2) {
+  if(val > 0) {
+    analogWrite(dir1, (val*2));
+    digitalWrite(dir2, LOW);
+  }
+  else if(val < 0) {
+    analogWrite(dir1, (257+(val*2)));
+    digitalWrite(dir2, HIGH);
+  } else {
+    digitalWrite(dir1, LOW);
+    digitalWrite(dir2, LOW);
+  }
+}
