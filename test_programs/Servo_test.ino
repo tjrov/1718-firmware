c1fca87656fd050bb4c720946a4186d9453bc5d5
henrynester <henrynester@gmail.com>
2018-03-24 18:10:57 -0400

    Update Servo_test.ino

--- test_programs/Servo_test.ino
+++ test_programs/Servo_test.ino
@@ -1,8 +1,17 @@
+//tests ROV manipulator servos using onboard H-bridge system
+//send a number from -128 to 127 over the tether serial port @ 115200 bit/s
+//the light will flash and the motor will do that command for a few seconds
+//the light will flash and it will stop again
+
 #include "pindefs.h"
 
+//change to use any motor in pindefs.h
+#define LOCAL_DIR1 MOT1_DIR1
+#define LOCAL_DIR2 MOT1_DIR2
+
 void setup() {
-  pinMode(MOT1_DIR1, OUTPUT);
-  pinMode(MOT1_DIR2, OUTPUT);
+  pinMode(LOCAL_DIR1, OUTPUT);
+  pinMode(LOCAL_DIR2, OUTPUT);
   pinMode(TX_EN, OUTPUT);
   pinMode(STATUS_LED, OUTPUT);
   digitalWrite(TX_EN, LOW);
@@ -15,7 +24,7 @@ void loop() {
   digitalWrite(STATUS_LED, HIGH);
   delay(50);
   digitalWrite(STATUS_LED, LOW);
-  setMotor(spd, MOT1_DIR1, MOT1_DIR2);
+  setMotor(spd, LOCAL_DIR1, LOCAL_DIR2);
   delay(2000);
 }
 
